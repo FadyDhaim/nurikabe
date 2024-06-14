d3d6c8056f1fc63819254fd4b9210a5bb50c7462
FadyDhaim <fadyaldhaim@gmail.com>
2024-06-14 09:50:54 +0300

    initial commit

---
+++ nurikabe.pl
@@ -0,0 +1,17 @@
+% fixed cells
+fxd_cell(1, 6, 3).
+fxd_cell(1, 8, 3).
+fxd_cell(3, 7, 3).
+fxd_cell(4, 2, 6).
+fxd_cell(5, 3, 2).
+fxd_cell(5, 7, 4).
+fxd_cell(6, 8, 3).
+fxd_cell(7, 3, 1).
+fxd_cell(9, 2, 6).
+fxd_cell(9, 4, 4).
+
+
+% solve
+solve_cell(1,1,blue).
+solve_cell(1,3,green).
+solve_cell(1,4,green).
