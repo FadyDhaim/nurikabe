41a8648edf10e8af17a8cee151783b2f7daef1b3
FadyDhaim <fadyaldhaim@gmail.com>
2024-06-15 16:32:00 +0300

    x

--- nurikabe_static_solution.pl
+++ nurikabe_static_solution.pl
@@ -40,63 +40,63 @@ solve_cell(3, 8, blue).
 solve_cell(3, 9, blue).
 
 % solve_cell(4, 1, green).
-solve_cell(4, 2, green).
-solve_cell(4, 3, green).
-solve_cell(4, 4, green).
-solve_cell(4, 5, green).
-solve_cell(4, 6, blue).
-solve_cell(4, 7, blue).
-solve_cell(4, 8, blue).
+solve_cell(4, 2, blue).
+% solve_cell(4, 3, green).
+solve_cell(4, 4, blue).
+% solve_cell(4, 5, green).
+solve_cell(4, 6, green).
+solve_cell(4, 7, green).
+solve_cell(4, 8, green).
 solve_cell(4, 9, blue).
 
 solve_cell(5, 1, green).
 solve_cell(5, 2, blue).
 solve_cell(5, 3, green).
 solve_cell(5, 4, blue).
-solve_cell(5, 5, green).
+solve_cell(5, 5, blue).
 solve_cell(5, 6, blue).
 solve_cell(5, 7, blue).
 solve_cell(5, 8, blue).
-solve_cell(5, 9, blue).
+solve_cell(5, 9, green).
 
 solve_cell(6, 1, blue).
-solve_cell(6, 2, blue).
-solve_cell(6, 3, blue).
+solve_cell(6, 2, green).
+solve_cell(6, 3, green).
 solve_cell(6, 4, blue).
-solve_cell(6, 5, green).
+% solve_cell(6, 5, green).
 solve_cell(6, 6, blue).
-solve_cell(6, 7, green).
+% solve_cell(6, 7, green).
 solve_cell(6, 8, blue).
-solve_cell(6, 9, green).
+% solve_cell(6, 9, green).
 
 solve_cell(7, 1, blue).
 solve_cell(7, 2, blue).
 solve_cell(7, 3, blue).
 solve_cell(7, 4, blue).
 solve_cell(7, 5, blue).
-solve_cell(7, 6, blue).
+solve_cell(7, 6, green).
 solve_cell(7, 7, green).
-solve_cell(7, 8, green).
-solve_cell(7, 9, green).
+solve_cell(7, 8, blue).
+solve_cell(7, 9, blue).
 
 solve_cell(8, 1, blue).
-solve_cell(8, 2, blue).
-solve_cell(8, 3, blue).
-solve_cell(8, 4, blue).
+solve_cell(8, 2, green).
+solve_cell(8, 3, green).
+solve_cell(8, 4, green).
 solve_cell(8, 5, blue).
-solve_cell(8, 6, green).
+solve_cell(8, 6, blue).
 solve_cell(8, 7, blue).
 solve_cell(8, 8, green).
-solve_cell(8, 9, blue).
+solve_cell(8, 9, green).
 
 solve_cell(9, 1, blue).
 solve_cell(9, 2, blue).
 solve_cell(9, 3, blue).
-solve_cell(9, 4, blue).
-solve_cell(9, 5, blue).
-solve_cell(9, 6, green).
-solve_cell(9, 7, green).
-solve_cell(9, 8, green).
+solve_cell(9, 4, green).
+solve_cell(9, 5, green).
+% solve_cell(9, 6, green).
+solve_cell(9, 7, blue).
+% solve_cell(9, 8, green).
 solve_cell(9, 9, green).
 
 % Entry point
